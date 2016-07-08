1764caa21d0e49340770b8e30c34b9adf4ed6f13
carsonlevine <carsonglevine@me.com>
2016-07-08 15:51:55 -0400

    first commit

---
+++ OOPractice2.playground/Contents.swift
@@ -0,0 +1,49 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//There are three common scales that are used to measure temperature: Celsius, Fahrenheit, and Kelvin
+//C = (F - 32) / 1.8
+//F = 1.8 * C + 32
+//K = C + 273
+
+//Level One: Add a
+//Make a struct called FreezingPoint that has three properties: celsius, fahrenheit, and kelvin. Give them all default values equal to the freezing point of water.
+
+
+
+
+//Level Two
+//Make a struct called Celsius that has three properties: celsius, fahrenheit, and kelvin. Give celsius a default value of 0.0, and make the values of fahrenheit and kelvin correct values, converted from the celsius property.
+
+
+
+
+
+//var tenDegreesCelsius = Celsius(celsius: 10.0)
+
+//tenDegreesCelsius.celsius //returns 10.0
+//tenDegreesCelsius.kelvin //returns 283.0
+//tenDegreesCelsius.fahrenheit //returns 50.0
+
+
+
+
+//Bonus: Give the Celsius struct a method called isBelowFreezing that returns a boolean value of true if the temperature is below freezing.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
